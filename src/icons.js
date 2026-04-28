fdeae1d69cf98699d77b986d309f1cac3e4dc93d
mishu <mishu@xtelecom.ro>
2026-04-28 18:12:29 +0300

    Register package icons

--- src/icons.js
+++ src/icons.js
@@ -1,4 +1,4 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { faCloudArrowUp, faFileExcel } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faCloudArrowUp);
+library.add(faCloudArrowUp, faFileExcel);
